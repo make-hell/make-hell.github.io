569acec62c976f07ff12da628cc88bc411d5d316
neeerajb <neerajb@sfu.ca>
2023-03-18 19:56:23 -0700

    Starting

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Neeraj Bansal</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/style.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="landing is-preload">
+        
